72304699149337905b9384b8923f90c11f77aa4b
henry-gu <henryguhb@outlook.com>
2021-12-15 10:39:22 +0800

    update payload

--- app.js
+++ app.js
@@ -11,21 +11,23 @@ const aboutContent = "About Event Notification Listener.";
 
 const events = [];
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   console.log(common.currentDateTime() + " HTTP GET: '/'");
   res.render("home", {
-    events: events
+    events: events,
   });
 });
 
-app.get("/event/:id", function(req, res) {
+app.get("/event/:id", function (req, res) {
   const requestId = req.params.id.replace(/-/g, "");
   console.log(common.currentDateTime() + " HTTP GET: '/eventlist/" + requestId + "'");
   events.forEach((eventItem) => {
@@ -33,39 +35,39 @@ app.get("/event/:id", function(req, res) {
     if (currentId === requestId) {
       res.render("event", {
         title: eventItem.title,
-        payload: eventItem.payload
+        payload: eventItem.payload,
       });
-      console.log("-- Display Event Payload: " + requestId)
+      console.log("-- Display Event Payload: " + requestId);
     }
   });
 });
 
-app.post("/eventlistener", function(req, res) {
-  const receiveDataTime = common.currentDateTime().slice(0,-4);
-  const eventId=req.body.id.replace(/-/g, "");
-  const eventTimeStamp = req.body.timeStamp.slice(0,-1);
+app.post("/eventlistener", function (req, res) {
+  const receiveDataTime = common.currentDateTime().slice(0, -4);
+  const eventId = req.body.id.replace(/-/g, "");
+  const eventTimeStamp = req.body.timeStamp.slice(0, -1);
   const eventType = req.body.eventType;
   const eventTopic = req.body.topic;
-  const eventFacts = JSON.stringify(req.body.facts);
+  const eventPayload = JSON.stringify(req.body, null, 4);
+  const eventFacts = JSON.stringify(req.body.facts, null, 4);
   const eventTitle = `[${eventTimeStamp}]-[${eventTopic}]-[${eventType}] `;
-  const eventSummary = `${eventFacts}`;
   console.log("Event Received at: " + receiveDataTime);
-  console.log("Event Summary:"+eventSummary);
+  console.log("Event Payload:" + eventPayload);
   const event = {
     id: eventId,
-    title: eventTitle ,
-    summary: eventSummary,
-    payload: req.body,
+    title: eventTitle,
+    summary: eventFacts,
+    payload: eventPayload,
   };
   events.push(event);
   res.send("Event Received.");
 });
 
 let port = process.env.PORT;
- if (port == null || port =="") {
-     port = 3030;
- }
+if (port == null || port == "") {
+  port = 3030;
+}
 
-app.listen(port, function(req, res) {
-  console.log("Server listening at port "+ port);
+app.listen(port, function (req, res) {
+  console.log("Server listening at port " + port);
 });
