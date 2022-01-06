7590e0110d3d8166df19a8e74008846ec1e5cbb1
henry-gu <henryguhb@outlook.com>
2022-01-06 16:42:10 +0800

    using envirnment variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -7,15 +8,16 @@ const app = express();
 const common = require(__dirname + "/common.js");
 const mongoose = require("mongoose");
 // const dbConnectUrl = "mongodb://localhost:27017/eventDB";
-const db_username = "admin-henrygu";
-const db_password = "KooGHB2021";
-const db_cluster = "cluster0.x4qhr";
-const db_name = "eventDB";
+// const db_username = "admin-henrygu";
+// const db_password = "KooGHB2021";
+// const db_cluster = "cluster0.x4qhr";
+// const db_name = "eventDB";
 
 // const connectString = "mongodb+srv://admin-henry:KooGHB2021@cluster0.x4qhr.mongodb.net/eventDB";
 
-const connectString =
-  "mongodb://admin-henry:KooGHB2021@cluster0-shard-00-00.x4qhr.mongodb.net:27017,cluster0-shard-00-01.x4qhr.mongodb.net:27017,cluster0-shard-00-02.x4qhr.mongodb.net:27017/eventDB?ssl=true&replicaSet=atlas-10ngp0-shard-0&authSource=admin&retryWrites=true&w=majority";
+const connectString = process.env.DB_CONNECT_STRING;
+// const connectString =
+//   "mongodb://admin-henry:KooGHB2021@cluster0-shard-00-00.x4qhr.mongodb.net:27017,cluster0-shard-00-01.x4qhr.mongodb.net:27017,cluster0-shard-00-02.x4qhr.mongodb.net:27017/eventDB?ssl=true&replicaSet=atlas-10ngp0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose.connect(connectString);
 
