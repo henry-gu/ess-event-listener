f58efad50788d6d0386b6d6fe50d07d8080f55dc
henry-gu <henryguhb@outlook.com>
2023-09-11 14:47:54 +0800

    req.connection

--- app.js
+++ app.js
@@ -96,17 +96,37 @@ app.post("/eventlistener", function (req, res) {
   let eventTimeStamp = common.getUTCDateTime().slice(0, -4);
   let eventType = req.body.eventType;
   let eventFactsHref = "";
+  let peerCommonName = "";
+  let peerSerialNumber = "";
 
   console.log(common.getUTCDateTime() + " >>> RECEIVED EVENT NOTIFICATION. ");
 
   const clientIpAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-  const cert = req.connect.getPeerCertificate();
-  if (cert.subject) {
-    console.log(common.getUTCDateTime() + `>>> CLIENT SUBJECT: ${cert.subject.CN}`);
+  const peerCertificate = req.connection.getPeerCertificate();
+
+
+  if (req.secure) {
+    // Extract the peer's certificate
+    // Extract Common Name (CN) and serial number
+    peerCommonName = peerCertificate.subject.CN || "N/A";
+    peerSerialNumber = peerCertificate.serialNumber || "N/A";
+    // Use commonName and serialNumber as needed
+  } else {
+    console.log(common.getUTCDateTime() + " >>> RECEIVED HTTP REQUEST.");
+    // For HTTP requests, retrieve the client certificate information from custom headers
+    peerCommonName = req.headers['x-ssl-client-s-dn-cn'] || "n/a";
+    peerSerialNumber = req.headers['x-ssl-client-serial-number'] || "n/a";
   }
-  if (cert.issuer) {
-    console.log(common.getUTCDateTime() + `>>> CLIENT CERT ISSUED BY: ${cert.issuer.CN}`);
+
+  console.log(common.getUTCDateTime() + ` >>> Common Name: ${peerCommonName}`);
+  console.log(common.getUTCDateTime() + ` >>> Serial Number: ${peerSerialNumber}`);
+
+  if (peerCertificate.subject) {
+    console.log(common.getUTCDateTime() + `>>> CLIENT SUBJECT: ${peerCertificate.subject.CN}`);
+  }
+  if (peerCertificate.issuer) {
+    console.log(common.getUTCDateTime() + `>>> CLIENT CERT ISSUED BY: ${peerCertificate.issuer.CN}`);
   }
 
   // Handle different event topics as needed
